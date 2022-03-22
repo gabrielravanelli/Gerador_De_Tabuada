57240a6e71824be7930ae7ed9238dabfbd337cb7
gabrielravanelli <gabrielravanellisimoes@gmail.com>
2022-03-22 18:11:30 -0300

    Código

---
+++ codigo/GeradorDeTabuada/src/geradordetabuada/GeradorDeTabuada.java
@@ -0,0 +1,32 @@
+package geradordetabuada;
+
+import java.util.Scanner;
+
+public class GeradorDeTabuada {
+
+	public static void main(String[] args) {
+		
+		int  contador, limite, resultado, tabuada;
+				
+		contador = 1;
+				
+		limite = 100; 
+				
+		Scanner scan = new Scanner(System.in);
+	    System.out.print("Digite qual tabuada você quer: ");
+		tabuada = scan.nextInt();
+				
+        do {//Faça
+	   	
+        	resultado  = tabuada * contador;
+        	System.out.println(tabuada + " x " + contador + " = " + resultado);
+        	contador ++;
+		
+        }while(contador <= limite);//while = enquanto
+				
+		
+		
+
+	}
+
+}
